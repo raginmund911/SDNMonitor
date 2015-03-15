c0333fe654869c9a1655ef23be7ef40b1aeacb7a
R. Rans <raymond.ransome@gmail.com>
2015-03-15 17:10:02 -0400

    Initial commit, project creation, added dependencies

---
+++ app/src/main/java/com/sdnmonitor/ransomer/MQService.java
@@ -0,0 +1,111 @@
+package com.sdnmonitor.ransomer;
+
+import android.app.IntentService;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.v4.content.AsyncTaskLoader;
+import android.util.Log;
+import android.widget.Toast;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.NoSuchElementException;
+
+/**
+ * Created by Ray on 4/2/15.
+ */
+public class MQService extends IntentService {
+    // Binder given to clients
+    //private final IBinder mBinder = new MQBinder();
+
+    /*
+    MQBinder provides getService() method for clients to retrieve the current
+    instance of MQService
+    */
+
+    public MQService() {
+        super("MQService");
+    }
+
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
+        super.onStartCommand(intent, flags, startId);
+        Toast.makeText(this, "MQService Started", Toast.LENGTH_LONG).show();
+
+        // We want this service to stop after it has run once.
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent)
+    {
+        Toast.makeText(this, "MQService Handling Intent", Toast.LENGTH_LONG).show();
+        // INSERT THE WORK TO BE DONE HERE
+
+        /*
+        factory.setHost("137.140.3.151");
+        factory.setUsername("ransomer");
+        factory.setPassword("*Sep91180");
+        factory.setPort(15672);
+        factory.setAutomaticRecoveryEnabled(true);
+        */
+        // connection that will recover automatically
+
+
+    }
+
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //The output TextView we'll use to display messages
+        //mOutput =  (TextView) findViewById(R.id.output);
+
+
+
+        //Connect to broker
+        //mConsumer.connectToRabbitMQ();
+
+
+
+
+        Log.d("MQService", "Service created");
+        Toast.makeText(this, "Service created ...", Toast.LENGTH_LONG).show();
+
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        Log.d("MQService", "Service Destroyed");
+
+
+    }
+
+
+    ConnectionFactory factory = new ConnectionFactory();
+    private void setupConnectionFactory() {
+
+        final URI uri = URI.create("amqp://test:test@137.140.3.151:15672/");
+
+        try {
+
+
+            factory.setUri(uri);
+        } catch (KeyManagementException | NoSuchAlgorithmException | URISyntaxException e1) {
+            e1.printStackTrace();
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/sdnmonitor/ransomer/SDNEventListActivity.java
@@ -0,0 +1,87 @@
+package com.sdnmonitor.ransomer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+
+/**
+ * An activity representing a list of SDNEvents. This activity
+ * has different presentations for handset and tablet-size devices. On
+ * handsets, the activity presents a list of items, which when touched,
+ * lead to a {@link SDNEventDetailActivity} representing
+ * item details. On tablets, the activity presents the list of items and
+ * item details side-by-side using two vertical panes.
+ * <p/>
+ * The activity makes heavy use of fragments. The list of items is a
+ * {@link SDNEventListFragment} and the item details
+ * (if present) is a {@link SDNEventDetailFragment}.
+ * <p/>
+ * This activity also implements the required
+ * {@link SDNEventListFragment.Callbacks} interface
+ * to listen for item selections.
+ */
+public class SDNEventListActivity extends FragmentActivity
+        implements SDNEventListFragment.Callbacks {
+
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private boolean mTwoPane;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sdnevent_list);
+
+        Log.d("SDNEventListActivity", "Activity created");
+        Log.d("SDNEventListActivity", "Attempting connection to RabbitMQ");
+        Toast.makeText(this, "Attempting connection to RabbitMQ ...", Toast.LENGTH_LONG).show();
+
+        if (findViewById(R.id.sdnevent_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-large and
+            // res/values-sw600dp). If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
+
+            // In two-pane mode, list items should be given the
+            // 'activated' state when touched.
+            ((SDNEventListFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.sdnevent_list))
+                    .setActivateOnItemClick(true);
+        }
+
+            startService(new Intent(this, MQService.class));
+    }
+
+    /**
+     * Callback method from {@link SDNEventListFragment.Callbacks}
+     * indicating that the item with the given ID was selected.
+     */
+    @Override
+    public void onItemSelected(String id) {
+        if (mTwoPane) {
+            // In two-pane mode, show the detail view in this activity by
+            // adding or replacing the detail fragment using a
+            // fragment transaction.
+            Bundle arguments = new Bundle();
+            arguments.putString(SDNEventDetailFragment.ARG_ITEM_ID, id);
+            SDNEventDetailFragment fragment = new SDNEventDetailFragment();
+            fragment.setArguments(arguments);
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.sdnevent_detail_container, fragment)
+                    .commit();
+
+        } else {
+            // In single-pane mode, simply start the detail activity
+            // for the selected item ID.
+            Intent detailIntent = new Intent(this, SDNEventDetailActivity.class);
+            detailIntent.putExtra(SDNEventDetailFragment.ARG_ITEM_ID, id);
+            startActivity(detailIntent);
+        }
+    }
+}
